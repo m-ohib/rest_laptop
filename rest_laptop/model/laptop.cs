4ed61dddd887b2ab2bb4968ac6226a2f15d6e848
m-ohib <mohammed_amiri@hotmail.dk>
2026-02-12 12:52:59 +0100

    model klassen er lavet med navnet Laptop og har værdierne Id,Price,Model. Der er lavet en tostring metode som returner Id,Price,Model

---
+++ rest_laptop/model/laptop.cs
@@ -0,0 +1,14 @@
+﻿namespace rest_laptop.model
+{
+    public class Laptop
+    {
+        public int Id { get; set; }
+        public int price { get; set; }
+        public string Model { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Price: {price}, Model: {Model}";
+        }
+    }
+}
